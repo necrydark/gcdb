00ec20a40bbdd521821126131eeebff95b2c7bdf
necrydark <necrydark@gmail.com>
2024-02-29 00:36:37 +0000

    added footer updated home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,45 @@
+"use client";
 import ThemeToggle from "@/components/ThemeToggle";
+import { motion } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="dark:bg-[#1a1b1f] h-full">
+    <div className="dark:bg-[#1a1b1f] h-screen">
       <div className="container mx-auto p-5">
         <div>
-          <h1 className="text-center text-white text-2xl">
+          <motion.h1
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1 }}
+            viewport={{ once: false }}
+            className="text-center text-white text-2xl p-2"
+          >
             Welcome to the Grand Cross Wiki
-          </h1>
+          </motion.h1>
+          <motion.p
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1.25 }}
+            viewport={{ once: false }}
+            className="text-center text-white text-lg p-2"
+          >
+            Grand Cross Wiki is a wiki for the Grand Cross community. We provide
+            all the latest information on the game as well as the correct
+            information for characters.
+          </motion.p>
+          <motion.p
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1.5 }}
+            viewport={{ once: false }}
+            className="text-center text-white text-lg p-2"
+          >
+            This information is up to date with the lastest version of the game
+            from KR and JP servers. This information is also used from
+            Youtubers.
+          </motion.p>
         </div>
       </div>
     </div>
