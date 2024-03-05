5b6f65dd6535faf72543f5a6026502dc8eb80571
necrydark <necrydark@gmail.com>
2024-03-05 19:48:35 +0000

    added definitions to pass in arrays in table comp

--- src/app/gear/page.tsx
+++ src/app/gear/page.tsx
@@ -3,27 +3,27 @@ import React from "react";
 
 function page() {
 
-  // const gears = [
-  //   {
-  //     gear: "Menacing Moon",
-  //     name: "Menacing Moon",
-  //     jpName: "脅威の月",
-  //     set: "Moon",
-  //     setBonus: "HP +10%",
-  //   },
-  //   {
-  //     gear: "Menacing Moon",
-  //     name: "Menacing Moon",
-  //     jpName: "脅威の月",
-  //     set: "Moon",
-  //     setBonus: "HP +10%",
-  //   },
-  // ]
+  const gears = [
+    {
+      gear: "Menacing Moon",
+      name: "Menacing Moon",
+      jpName: "脅威の月",
+      set: "Moon",
+      setBonus: "HP +10%",
+    },
+    {
+      gear: "Menacing Moon",
+      name: "Menacing Moon",
+      jpName: "脅威の月",
+      set: "Moon",
+      setBonus: "HP +10%",
+    },
+  ]
   return (
     <div className="container mx-auto p-10 space-y-5">
       <Table
         headers={["Gear", "JP", "EN", "Set", "Set Bonus"]}
-        data={["Menacing Moon", "脅威の月", "Menacing Moon", "Moon", "HP +10%"]}
+        data={gears}
       />
     </div>
   );
--- src/components/table.tsx
+++ src/components/table.tsx
@@ -5,7 +5,7 @@ interface IProps {
   data: any[];
 }
 
-const Table: React.FC<IProps> = ({ headers, data }) => {
+const Table: React.FC<IProps> = ({ headers, data }: {headers: any[], data: any[]}) => {
   return (
     <table className="w-full text-sm  rtl:text-right text-gray-500 dark:text-white font-bold">
       <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
